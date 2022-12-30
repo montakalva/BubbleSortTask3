667a20f37db27a2b771ed08a593526667f3a3d01
Kalva <monta.kalva@accenture.com>
2022-12-30 17:01:08 +0200

    first commit

---
+++ src/main/java/org/example/BubbleSorting.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Arrays;
+/*
+* Task 3: Write bubble sort using only java simple arrays ( type of sorting data: int)
+3.1) Think about complexity value of your sorting method ( O(?) )
+* */
+public class BubbleSorting {
+
+    int [] array = {17, 99, 10, 25, 2, 78};
+    int temp;
+
+    public void sorting(){
+        System.out.println("Array to sort: " + Arrays.toString(array));
+
+        for(int i = 0; i < array.length; i++){
+            for(int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println("Array after sorting: " + Arrays.toString(array));
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/*
+* Task 3: Write bubble sort using only java simple arrays ( type of sorting data: int)
+3.1) Think about complexity value of your sorting method ( O(?) )
+
+* */
+public class Main {
+    public static void main(String[] args) {
+
+        BubbleSorting bubbleSorting = new BubbleSorting();
+
+        bubbleSorting.sorting();
+
+    }
+}
